e0279f5d5520dbacbeacc39487c7f6d5e3a002fe
atul <atulnkhairnar@gmail.com>
2016-12-18 23:05:45 +0530

    mainController Added. Home page now contains posts

--- scripts/directives/home/home.js
+++ scripts/directives/home/home.js
@@ -1,4 +1,10 @@
-angular.module('lokytControllers', ['ngRoute'])
-	.controller('mainController', function($scope) {
-	    
-	})
+angular.module('lokytControllers', [])
+	.controller('mainController', ['$scope', '$http', function($scope, $http) {
+		var url = 'jsons/home.json';
+	    $http.get(url).success(
+	    	function(data) {
+	    		$scope.posts = data;
+	    }).error(function(data) {
+	    	console.log(data);
+	    });
+	}]);
