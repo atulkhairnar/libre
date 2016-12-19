01a6f8b75afa14f5e44b59c55912a43564236d8b
Atul K <atulnkhairnar@gmail.com>
2016-12-19 22:29:53 +0530

    upcoming event box is now updates dynamically

--- scripts/directives/home/home.js
+++ scripts/directives/home/home.js
@@ -1,13 +1,25 @@
 angular.module('lokytControllers', [])
 	.controller('mainController', ['$scope', '$http', function($scope, $http) {
 		var url = 'jsons/home.json';
+		$scope.noUpcomingEvent = true;   // no upcoming events
 	    $http.get(url).success(
 	    	function(data) {
 	    		$scope.posts = data;
-	    }).error(function(data) {
+	    		// filter out list of upcoming events
+	    		$scope.upcomingEvents = data.filter(function(e) {
+	    			if(e.type == "event" && new Date(e.eventDateTime) > new Date()) {
+	    				return e;
+	    			}
+	    		});
+	    		// set noUpcomingEvent value 
+	    		if($scope.upcomingEvents.length) {	
+	    			$scope.noUpcomingEvent = false;
+	    			$scope.eventImg = $scope.upcomingEvents[0].img;
+	    		}
+	    	}
+	    ).error(function(data) {
 	    	console.log(data);
 	    });
 
 	    $scope.downloadBook = "img/book/nfaoe.jpg";
-	    $scope.eventImg = "img/event/atmaswar.jpg";
 	}]);
